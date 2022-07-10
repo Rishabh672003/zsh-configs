2a8b654e59a26a1f731a6c5d84bca4d1a73244da
Rishabh <jharishabh2k3@gmail.com>
2022-07-10 20:58:12 +0530

    added a script to install zsh4humans

---
+++ z4h-setup/install-z4h.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if command -v curl >/dev/null 2>&1; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+else
+  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+fi
