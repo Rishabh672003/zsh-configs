df1e6c75b814a09e535f4e12cf28543f2398970a
Rishabh <jharishabh2k3@gmail.com>
2022-07-11 16:13:11 +0530

    updated stuff

--- z4h-setup/install-z4h.sh
+++ z4h-setup/install-z4h.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if command -v curl >/dev/null 2>&1; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 else
-  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+    sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 fi
