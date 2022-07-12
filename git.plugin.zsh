365964e52392881938a602217f16c3cf4b5abdef
Rishabh <jharishabh2k3@gmail.com>
2022-07-12 11:47:32 +0530

    formatted the file

--- git.plugin.zsh
+++ git.plugin.zsh
@@ -11,46 +11,46 @@ git_version="${${(As: :)$(git version 2>/dev/null)}[3]}"
 # the plugin, before being pulled in to core lib/git.zsh as git_current_branch()
 # to fix the core -> git plugin dependency.
 function current_branch() {
-  git_current_branch
+    git_current_branch
 }
 
 # Pretty log messages
 function _git_log_prettily(){
-  if ! [ -z $1 ]; then
-    git log --pretty=$1
-  fi
+    if ! [ -z $1 ]; then
+        git log --pretty=$1
+    fi
 }
 compdef _git _git_log_prettily=git-log
 
 # Warn if the current branch is a WIP
 function work_in_progress() {
-  command git -c log.showSignature=false log -n 1 2>/dev/null | grep -q -- "--wip--" && echo "WIP!!"
+    command git -c log.showSignature=false log -n 1 2>/dev/null | grep -q -- "--wip--" && echo "WIP!!"
 }
 
 # Check if main exists and use instead of master
 function git_main_branch() {
-  command git rev-parse --git-dir &>/dev/null || return
-  local ref
-  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
-    if command git show-ref -q --verify $ref; then
-      echo ${ref:t}
-      return
-    fi
-  done
-  echo master
+    command git rev-parse --git-dir &>/dev/null || return
+    local ref
+    for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
+        if command git show-ref -q --verify $ref; then
+            echo ${ref:t}
+            return
+        fi
+    done
+    echo master
 }
 
 # Check for develop and similarly named branches
 function git_develop_branch() {
-  command git rev-parse --git-dir &>/dev/null || return
-  local branch
-  for branch in dev devel development; do
-    if command git show-ref -q --verify refs/heads/$branch; then
-      echo $branch
-      return
-    fi
-  done
-  echo develop
+    command git rev-parse --git-dir &>/dev/null || return
+    local branch
+    for branch in dev devel development; do
+        if command git show-ref -q --verify refs/heads/$branch; then
+            echo $branch
+            return
+        fi
+    done
+    echo develop
 }
 
 #
@@ -97,8 +97,8 @@ alias gcb='git checkout -b'
 alias gcf='git config --list'
 
 function gccd() {
-  command git clone --recurse-submodules "$@"
-  [[ -d "$_" ]] && cd "$_" || cd "${${_:t}%.git}"
+    command git clone --recurse-submodules "$@"
+    [[ -d "$_" ]] && cd "$_" || cd "${${_:t}%.git}"
 }
 compdef _git gccd=git-clone
 
@@ -128,7 +128,7 @@ alias gdup='git diff @{upstream}'
 alias gdw='git diff --word-diff'
 
 function gdnolock() {
-  git diff "$@" ":(exclude)package-lock.json" ":(exclude)*.lock"
+    git diff "$@" ":(exclude)package-lock.json" ":(exclude)*.lock"
 }
 compdef _git gdnolock=git-diff
 
@@ -138,8 +138,8 @@ compdef _git gdv=git-diff
 alias gf='git fetch'
 # --jobs=<n> was added in git 2.8
 is-at-least 2.8 "$git_version" \
-  && alias gfa='git fetch --all --prune --jobs=10' \
-  || alias gfa='git fetch --all --prune'
+    && alias gfa='git fetch --all --prune --jobs=10' \
+    || alias gfa='git fetch --all --prune'
 alias gfo='git fetch origin'
 
 alias gfg='git ls-files | grep'
@@ -148,48 +148,48 @@ alias gg='git gui citool'
 alias gga='git gui citool --amend'
 
 function ggf() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git push --force origin "${b:=$1}"
+    [[ "$#" != 1 ]] && local b="$(git_current_branch)"
+    git push --force origin "${b:=$1}"
 }
 compdef _git ggf=git-checkout
 function ggfl() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git push --force-with-lease origin "${b:=$1}"
+    [[ "$#" != 1 ]] && local b="$(git_current_branch)"
+    git push --force-with-lease origin "${b:=$1}"
 }
 compdef _git ggfl=git-checkout
 
 function ggl() {
-  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-    git pull origin "${*}"
-  else
-    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
-    git pull origin "${b:=$1}"
-  fi
+    if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
+        git pull origin "${*}"
+    else
+        [[ "$#" == 0 ]] && local b="$(git_current_branch)"
+        git pull origin "${b:=$1}"
+    fi
 }
 compdef _git ggl=git-checkout
 
 function ggp() {
-  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-    git push origin "${*}"
-  else
-    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
-    git push origin "${b:=$1}"
-  fi
+    if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
+        git push origin "${*}"
+    else
+        [[ "$#" == 0 ]] && local b="$(git_current_branch)"
+        git push origin "${b:=$1}"
+    fi
 }
 compdef _git ggp=git-checkout
 
 function ggpnp() {
-  if [[ "$#" == 0 ]]; then
-    ggl && ggp
-  else
-    ggl "${*}" && ggp "${*}"
-  fi
+    if [[ "$#" == 0 ]]; then
+        ggl && ggp
+    else
+        ggl "${*}" && ggp "${*}"
+    fi
 }
 compdef _git ggpnp=git-checkout
 
 function ggu() {
-  [[ "$#" != 1 ]] && local b="$(git_current_branch)"
-  git pull --rebase origin "${b:=$1}"
+    [[ "$#" != 1 ]] && local b="$(git_current_branch)"
+    git pull --rebase origin "${b:=$1}"
 }
 compdef _git ggu=git-checkout
 
@@ -280,8 +280,8 @@ alias gst='git status'
 
 # use the default stash push on git 2.13 and newer
 is-at-least 2.13 "$git_version" \
-  && alias gsta='git stash push' \
-  || alias gsta='git stash save'
+    && alias gsta='git stash push' \
+    || alias gsta='git stash save'
 
 alias gstaa='git stash apply'
 alias gstc='git stash clear'
@@ -321,17 +321,17 @@ alias gama='git am --abort'
 alias gamscp='git am --show-current-patch'
 
 function grename() {
-  if [[ -z "$1" || -z "$2" ]]; then
-    echo "Usage: $0 old_branch new_branch"
-    return 1
-  fi
+    if [[ -z "$1" || -z "$2" ]]; then
+        echo "Usage: $0 old_branch new_branch"
+        return 1
+    fi
 
-  # Rename branch locally
-  git branch -m "$1" "$2"
-  # Rename branch in origin remote
-  if git push origin :"$1"; then
-    git push --set-upstream origin "$2"
-  fi
+    # Rename branch locally
+    git branch -m "$1" "$2"
+    # Rename branch in origin remote
+    if git push origin :"$1"; then
+        git push --set-upstream origin "$2"
+    fi
 }
 
 unset git_version
