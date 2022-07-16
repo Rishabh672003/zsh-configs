2c093b4632ed05ba4496506d1dc17b55ee4754a3
Rishabh <jharishabh2k3@gmail.com>
2022-07-16 21:08:59 +0530

    updated stuff

--- aliases.zsh
+++ aliases.zsh
@@ -1,27 +1,28 @@
 #my aliases
 alias cal='nvim ~/.config/zsh/aliases.zsh'
 alias nr='sudo systemctl restart NetworkManager'
-alias y='yay' 
-alias ys='yay -Syu --noconfirm'
+alias y='yay'
+alias ys='yay -Syu --noconfirm --devel'
 alias p='paru'
 alias n='neofetch'
 alias f='fastfetch'
 alias c='clear'
 alias cd='z'
 alias cdd='zi'
+alias cdp='cd ..'
+alias cdpp='cd ../../'
 alias cz='nvim ~/.config/zsh/.zshrc'
 alias e='exit'
 alias vs='sudoedit'
 alias ae='alacritty -e'
 alias lg='lazygit'
-alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
-alias v='nvim'
+alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
+alias nv='nvim'
 alias nnvim='~/Applications/nvim.appimage'
-alias hx='helix' 
+alias hx='helix'
 alias cat='bat -p'
 alias cn='c && n'
 alias cnp='c;n;p'
-alias cdp='cd ..'
 alias cny='c;n;y'
 alias cf='clear;fastfetch'
 alias shut='shutdown now'
@@ -48,16 +49,14 @@ alias wtr='curl -4 https://wttr.in/virar'
 alias ua-drop-caches='yay -Sc --aur --noconfirm'
 alias ua-update-all='export TMPFILE="$(mktemp)"; \
     sudo true; \
-    rate-mirrors --disable-comments --save=$TMPFILE arch --max-delay=21600 \
-      && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
-      && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
-      && ua-drop-caches \
-      && yay -Syyu --noconfirm'
-alias ngit='cp -r /home/rishabh/.config/nvim/* ~/projects/Neovim/ && cd ~/projects/Neovim'
-alias neo='cp ~/Projects/Neovim/README.md ~/.config/nvim/ ; cp -r ~/.config/nvim/* ~/projects/Neovim/ && z ~/Projects/Neovim'
-alias zshc='cp -r ~/.config/zsh/* ~/projects/zsh-configs/ ; cd ~/projects/zsh-configs'
-alias awe='cd ~/projects/awesome-dotfiles/ && git pull && git submodule update --remote --merge \
-  && cp -r ~/.config/awesome/* ~/projects/awesome-dotfiles/config/awesome/ \
-  && z ~/projects/awesome-dotfiles/config/awesome && rm user_variables.lua \
-  && cp ~/projects/user_variables.lua ~/projects/awesome-dotfiles/config/awesome/ \
-  && cd ~/projects/awesome-dotfiles/ '
+    rate-mirrors --disable-comments --save=$TMPFILE arch --max-delay=21600 && \
+    sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup && \
+    sudo mv $TMPFILE /etc/pacman.d/mirrorlist && \
+    ua-drop-caches && \
+    yay -Syyu --noconfirm'
+alias ngit='cp -r /home/rishabh/.config/nvim/* ~/projects/Neovim/ && \
+    cd ~/projects/Neovim'
+alias neo='cp ~/Projects/Neovim/README.md ~/.config/nvim/ ; \
+    cp -r ~/.config/nvim/* ~/projects/Neovim/ && z ~/Projects/Neovim'
+alias zshc='cp -r ~/.config/zsh/* ~/projects/zsh-configs/ ; \
+    cd ~/projects/zsh-configs'
