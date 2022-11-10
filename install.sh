d9a870ef019f626311788e86914d6db1226289be
Rishabh <jharishabh2k3@gmail.com>
2022-11-10 21:13:19 +0530

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee /etc/zsh/zshenv &&  
+echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee -a /etc/zsh/zshenv &&  
 sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh) &&
 git clone https://github.com/Rishabh672003/zsh-configs ~/.config/zsh &&
 exec zsh
