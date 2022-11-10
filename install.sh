6a39683efb82b8e7b924ea8fdb6273c4aa377db0
Rishabh <jharishabh2k3@gmail.com>
2022-11-10 21:24:37 +0530

    update

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee -a /etc/zsh/zshenv &&  
 sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh) &&
 git clone https://github.com/Rishabh672003/zsh-configs ~/.config/zsh &&
-mkdir "$HOME"/.local/state/zsh && touch "$HOME"/.local/state/zsh/history;
+mkdir "$HOME"/.local/state/zsh && touch "$HOME"/.local/state/zsh/history &&
 exec zsh 
 
